518b05776e88d09b3a9e294a44361d849dcb2f76
krovorgen <krovorgen@gmail.com>
2020-12-07 00:56:59 +0300

    2.6Data-type-classification

---
+++ src/2.6Data-type-classification/index.js
@@ -0,0 +1,14 @@
+const number = 4; // Number
+console.log(4 / 0); // Infinity
+console.log("string" * 9); // NaN
+const persone = "Maksim"; // String
+const bool = true; // Boolean
+// null
+let und;
+console.log(und); // Undefined
+const obj = {
+  name: "Maksim",
+  age: 22,
+}; // Object
+const arr = [1, 2, 3];
+console.log(number, persone, bool, obj.name, arr);
